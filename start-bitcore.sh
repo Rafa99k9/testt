e14e75da819532dd089dbf340df5b61c54c7c117
Rafa99k9 <114609997+Rafa99k9@users.noreply.github.com>
2022-09-28 15:09:26 -0300

    Add files via upload

---
+++ start-bitcore.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+while true
+do
+./wildrig-multi --print-full --algo megabtx --url stratum+tcp://stratum-eu.rplant.xyz:17066 --user sY7v3ieNguMPRd8XkzGdUtASDZFrCvdAmn --pass x
+sleep 5
+done
